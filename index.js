945ab5ac48155287ad67e6b586361a0973eee79c
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-07-26 15:17:05 +0200

    Add ecd setup

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 /* jshint node: true */
 'use strict';
+var notifier = require('node-notifier');
 
 module.exports = {
-  name: 'ember-cli-deploy-notifier'
+  name: 'ember-cli-deploy-notifier',
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      didBuild: function(context) {
+        //do something amazing here once the project has been built
+      },
+
+      upload: function(context) {
+        //do something here to actually deploy your app somewhere
+      },
+
+      didDeploy: function(context) {
+        //do something here like notify your team on slack
+      }
+    };
+  }
 };
